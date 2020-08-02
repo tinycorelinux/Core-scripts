16ed63f5e90ab752149000543485ce4f5e8daf7f
paul-1 <6473457+paul-1@users.noreply.github.com>
2020-08-02 11:45:59 -0400

    bootlocal.sh: Base for piCore

--- opt/bootlocal.sh
+++ opt/bootlocal.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-# put other system startup commands here
+
+# To start serial terminal /w console
+# Add the following to the cmdline.txt console=serial0,115200
+# Reference https://www.raspberrypi.org/documentation/configuration/uart.md for UART configuration
+# Uncomment the next line
+# /usr/sbin/startserialtty &
+
+# Set CPU frequency governor to ondemand (default is performance)
+echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
+# Load modules - i2c-dev needs manually loaded even if enabled in config.txt
+#/sbin/modprobe i2c-dev
+
+# Start openssh daemon
+/usr/local/etc/init.d/openssh start
+
+# ------ Put other system startup commands below this line
